c307063c505146d30bc63fbebba052ee513a4d6c
Sujin <88263178+diveintoo@users.noreply.github.com>
2024-07-31 22:00:06 +0900

    Solve 취소되지 않은 진료 예약 조회하기.sql

---
+++ Programmers/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132204
+
+SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
+FROM APPOINTMENT a
+JOIN PATIENT p ON a.PT_NO = p.PT_NO
+JOIN DOCTOR d ON a.MDDR_ID = d.DR_ID
+WHERE a.APNT_CNCL_YN = 'N' AND a.MCDP_CD = 'CS' AND a.APNT_YMD LIKE '2022-04-13%'
+ORDER BY APNT_YMD;
